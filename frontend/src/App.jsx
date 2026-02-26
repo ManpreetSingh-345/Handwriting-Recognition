8b54dcbc61e41b8683467f1a6b6a607dbc752ea6
HanXD345 <manpreet345@yahoo.com>
2026-02-26 18:06:06 -0500

    Add grid background and configurations

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,56 @@
 import React from "react";
 import uploadIcon from "./assets/upload-icon.svg";
+import gridBg from "./assets/grid.png";
+import { GridPattern } from "@/components/ui/grid-pattern";
 
 function App() {
   return (
-    <div className="bg-size-[100vh] h-screen bg-black text-white box-border">
+    <div className="text-white box-border">
       {/* Header */}
-      <div className="flex justify-between items-center px-20 py-10 pb-25 font-space-mono">
-        <div className=" font-bold text-[38px] text-red-700">SMART WRITE</div>
-        <div className="flex justify-between gap-10">
-          <button className="hover:cursor-pointer">Dashboard</button>
-          <button className="hover:cursor-pointer">API</button>
-          <button className="hover:cursor-pointer">Authors</button>
-          <button className="hover:cursor-pointer">Contact</button>
-        </div>
-      </div>
+
       {/* Hero */}
-      <div className="flex justify-center items-center font-neue-machina flex-col gap-20">
-        <div className="text-4xl">
-          Ultimate <span className="text-red-400">Writing</span> Recognition
-        </div>
-        <div className="border rounded-lg min-w-xl min-h-30 grid grid-cols-[2fr_1fr] place-items-center divide-x">
-          <div className="py-20 px-30 flex flex-col gap-4 justify-center items-center">
-            <img
-              src={uploadIcon}
-              alt="Upload Icon"
-              className="hover:cursor-pointer"
-            />
-            <div>Draw or upload your image here</div>
+      <div className="border-0 bg-background relative flex flex-col gap-10 overflow-hidden rounded-lg p-10 z-[-1]">
+        <GridPattern
+          width={100}
+          height={100}
+          x={-1}
+          y={-1}
+          strokeDasharray={""}
+          className="bg-linear-to-b from-20% from-[#111111] to-80% to-[#434343]"
+        />
+        <div className="relative flex justify-between items-center px-20 py-10 font-space-mono">
+          <div className=" font-bold text-[38px] text-red-700">SMART WRITE</div>
+          <div className="flex justify-between gap-10">
+            <button className="hover:cursor-pointer">Dashboard</button>
+            <button className="hover:cursor-pointer">API</button>
+            <button className="hover:cursor-pointer">Authors</button>
+            <button className="hover:cursor-pointer">Contact</button>
           </div>
-          <div>Result</div>
+        </div>
+        <div className="relative z-1 flex justify-center items-center font-neue-machina flex-col gap-20">
+          <div className="text-4xl">
+            Ultimate <span className="text-red-400">Writing</span> Recognition
+          </div>
+          {/* Input/Results Section */}
+          <div className="border rounded-lg m-5 min-w-xl min-h-50 grid grid-cols-[2fr_1fr] place-items-center divide-x">
+            <div className="py-30 px-50 flex flex-col gap-4 justify-center items-center">
+              <img
+                src={uploadIcon}
+                alt="Upload Icon"
+                className="hover:cursor-pointer"
+              />
+              <div>Draw or upload your image here</div>
+            </div>
+            <div className="flex flex-col items-center h-full py-2">
+              <div className="flex-1">RESULT</div>
+              <div className="flex-1 text-gray-500">
+                RESULTS WILL APPEAR HERE
+              </div>
+              <div>
+                <button className="flex-1">View in Dashboard</button>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
