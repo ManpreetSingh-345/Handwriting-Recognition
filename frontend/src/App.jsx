01c9dac2da5dab5c84306a2ff24a9ceafbc8f5e0
HanXD345 <manpreet345@yahoo.com>
2026-02-26 20:33:30 -0500

    Add article sections

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import uploadIcon from "./assets/upload-icon.svg";
+import tabsIcon from "./assets/tabs.png";
 import { GridPattern } from "@/components/ui/grid-pattern";
 
 function App() {
   return (
-    <div className="text-white box-border">
-      <div className="border-0 bg-background relative flex flex-col gap-10 overflow-hidden p-10 z-0">
+    <main className="text-white font-neue-machina box-border">
+      <section className="border-0 bg-background relative flex flex-col gap-10 overflow-hidden p-10 z-0">
         <GridPattern
           width={70}
           height={70}
@@ -15,19 +16,19 @@ function App() {
           className="bg-linear-to-b from-20% from-[#111111] to-80% to-[#434343]"
         />
         {/* Header */}
-        <div className="relative flex justify-between items-center px-20 py-10 font-space-mono">
+        <header className="relative flex justify-between items-center px-20 py-10 font-space-mono">
           <div className=" font-bold text-[38px] text-[#ff5757]">
             SMART WRITE
           </div>
-          <div className="flex justify-between gap-10">
+          <nav className="flex justify-between gap-10">
             <button className="hover:cursor-pointer">Dashboard</button>
             <button className="hover:cursor-pointer">API</button>
             <button className="hover:cursor-pointer">Authors</button>
             <button className="hover:cursor-pointer">Contact</button>
-          </div>
-        </div>
+          </nav>
+        </header>
         {/* Hero */}
-        <div className="relative z-1 flex justify-center items-center font-neue-machina flex-col gap-20">
+        <section className="relative z-1 flex justify-center items-center font-neue-machina flex-col gap-20">
           <div className="text-4xl">
             Ultimate <span className="text-[#ff5757]">Writing</span> Recognition
           </div>
@@ -57,17 +58,18 @@ function App() {
               </div>
             </div>
           </div>
-        </div>
-      </div>
-      <div className="border-0 bg-background relative flex flex-col gap-10 overflow-hidden p-20 z-0">
+        </section>
+      </section>
+      <section className="border-0 bg-background relative flex flex-col gap-10 overflow-hidden p-20 z-0">
         <GridPattern
           width={70}
           height={70}
           x={-1}
           y={-1}
           strokeDasharray={""}
-          className="bg-linear-to-t from-20% from-[#111111] to-80% to-[#434343]"
+          className="bg-linear-to-t from-black to-50% to-[#434343]"
         />
+        {/* Metric Section */}
         <div className="relative flex flex-wrap justify-center gap-50 font-neue-machina text-7xl font-bold *:flex *:flex-col *:items-center *:gap-5">
           <div>
             <div>
@@ -82,8 +84,55 @@ function App() {
             <div className="text-4xl">USERS</div>
           </div>
         </div>
-      </div>
-    </div>
+        <article className="relative font-neue-machina p-20 grid grid-cols-2 items-center">
+          <div className="flex flex-col gap-5">
+            <div className="text-4xl">
+              Ease of <span className="underline text-[#ff5757]">Use</span>
+            </div>
+            <div className="font-space-mono">
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it to make a type specimen book.
+            </div>
+          </div>
+          <div className="flex justify-center">
+            <img src={tabsIcon} alt="Tabs" />
+          </div>
+        </article>
+      </section>
+      <section className="bg-radial-[at_500%_50%] from-50% from-[#f84e50] to-70% to-black">
+        <article className="flex justify-center py-20">
+          <div className="flex flex-col text-center min-w-100 max-w-200 gap-5">
+            <div className="text-3xl">
+              Developed by{" "}
+              <span className="underline text-[#ff5757]">Students</span>
+            </div>
+            <div className="text-1xl">
+              This application was fully developed by students as part of their
+              journey toward becoming software engineers. What began as a simple
+              idea evolved into a full-fledged application with features and
+              design principles aligned with industry standards, demonstrating
+              both technical skill and real-world applicability.
+            </div>
+            <div>
+              <button className="hover:cursor-pointer bg-[#202020] px-5 py-2 rounded-3xl text-sm">
+                Authors
+              </button>
+            </div>
+          </div>
+        </article>
+        <section className="flex flex-col items-center gap-5 bg-radial-[at_-500%_50%] from-50% from-[#f84e50] to-70% to-black">
+          <div className="text-3xl">
+            <span className="text-[#ff5757]">API</span> Integration
+          </div>
+          <div>
+            Our industry-leading models are designed for real-world utility.
+          </div>
+          <div></div>
+        </section>
+      </section>
+    </main>
   );
 }
 
