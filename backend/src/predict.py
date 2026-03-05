0488dbf86a5999b53ac4068a4f68354bfa1ffa9f
Ridhwan Muntasir <zeroascend@Ridhwans-MacBook-Air.local>
2026-03-05 17:39:00 -0500

    Fix Mac Errors

--- backend/src/predict.py
+++ backend/src/predict.py
@@ -3,8 +3,12 @@ from PIL import Image
 from model import CharacterCNN
 from preprocess import transform 
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
+if torch.cuda.is_available():
+    device = torch.device("cuda")
+elif torch.backends.mps.is_available():
+    device = torch.device("mps")
+else:
+    device = torch.device("cpu")
 # Load the blueprint and the best weights
 model = CharacterCNN().to(device)
 checkpoint = torch.load("best_character_model.pth", map_location=device, weights_only=False)
--- backend/src/train.py
+++ backend/src/train.py
@@ -10,7 +10,12 @@ from model import CharacterCNN
 #im LOSING IT
 #im going to crack this code >:D
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu") # decides what device to use for training (GPU if available, otherwise CPU)
+if torch.cuda.is_available():
+    device = torch.device("cuda")
+elif torch.backends.mps.is_available():
+    device = torch.device("mps")
+else:
+    device = torch.device("cpu") # decides what device to use for training (GPU if available, otherwise CPU)
 
 transform = transforms.Compose([ # custom pipeline to match EMNIST formatting, includes augmentation to improve generalization
     transforms.Grayscale(num_output_channels=1),                                  # convert to grayscale
@@ -48,81 +53,82 @@ num_workers = 0 if os.name == 'nt' else multiprocessing.cpu_count() # this is se
 train_loader = DataLoader(train_dataset, batch_size=256, shuffle=True, num_workers=num_workers, pin_memory=True)  # 256 images per batch, num_workers=cpu cores available, pin_memory speeds up CPU->GPU transfer
 val_loader = DataLoader(val_dataset, batch_size=256, shuffle=False, num_workers=num_workers, pin_memory=True)     # no shuffle for validation so results are consistent
 
-model = CharacterCNN().to(device) # creates the model and sends it to the device (GPU or CPU)
-criterion = nn.CrossEntropyLoss() # calculates how wrong the model is by comparing its output to the correct answer
+if __name__ == '__main__':
+    model = CharacterCNN().to(device) # creates the model and sends it to the device (GPU or CPU)
+    criterion = nn.CrossEntropyLoss() # calculates how wrong the model is by comparing its output to the correct answer
 
-checkpoint_path = "best_character_model.pth"
-if os.path.exists(checkpoint_path):
-    print(f"Loading existing weights from {checkpoint_path}...")
-    checkpoint = torch.load(checkpoint_path, weights_only=False)
-    model.load_state_dict(checkpoint["model_state"]) # load the brain from the hard drive into the model
-    best_accuracy = checkpoint["accuracy"]           # load the best accuracy so we dont overwrite a better model
-    print(f"Resuming from accuracy: {best_accuracy:.2f}%")
-else:
-    print("No saved model found. Starting from scratch!")
-    best_accuracy = 0.0
+    checkpoint_path = "best_character_model.pth"
+    if os.path.exists(checkpoint_path):
+        print(f"Loading existing weights from {checkpoint_path}...")
+        checkpoint = torch.load(checkpoint_path, map_location=device, weights_only=False)
+        model.load_state_dict(checkpoint["model_state"]) # load the brain from the hard drive into the model
+        best_accuracy = checkpoint["accuracy"]           # load the best accuracy so we dont overwrite a better model
+        print(f"Resuming from accuracy: {best_accuracy:.2f}%")
+    else:
+        print("No saved model found. Starting from scratch!")
+        best_accuracy = 0.0
 
-optimizer = optim.Adam(model.parameters(), lr=0.0005, weight_decay=1e-5)                               # L2 regularization to prevent overfitting
-scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', factor=0.5, patience=2) # reduce learning rate if validation loss doesnt improve for 2 epochs
+    optimizer = optim.Adam(model.parameters(), lr=0.0005, weight_decay=1e-5)                               # L2 regularization to prevent overfitting
+    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', factor=0.5, patience=2) # reduce learning rate if validation loss doesnt improve for 2 epochs
 
-epochs = 20 # makes the model go through the dataset 5 times
+    epochs = 20 # makes the model go through the dataset 5 times
 
 
-for epoch in range(epochs):
+    for epoch in range(epochs):
 
-    model.train() # tells model to train
-    running_loss = 0.0 # starts loss count from 0
-    correct = 0 # starts correct count from 0
-    total = 0 # starts total count from 0
+        model.train() # tells model to train
+        running_loss = 0.0 # starts loss count from 0
+        correct = 0 # starts correct count from 0
+        total = 0 # starts total count from 0
 
-    for images, labels in train_loader: # takes 256 images per batch
-        images, labels = images.to(device), labels.to(device) # decides what device to use
+        for images, labels in train_loader: # takes 256 images per batch
+            images, labels = images.to(device), labels.to(device) # decides what device to use
 
-        optimizer.zero_grad()              # clears the old errors
-        outputs = model(images)            # sends images through
-        loss = criterion(outputs, labels)  # checks answers
-        loss.backward()                    # weighs how wrong it was
-        optimizer.step()                   # changes parameter to get right next time
+            optimizer.zero_grad()              # clears the old errors
+            outputs = model(images)            # sends images through
+            loss = criterion(outputs, labels)  # checks answers
+            loss.backward()                    # weighs how wrong it was
+            optimizer.step()                   # changes parameter to get right next time
 
-        running_loss += loss.item()                    # adds error loss weight and sees total
-        _, predicted = torch.max(outputs.data, 1)     # figures out what the model thinks it is
-        total += labels.size(0)
-        correct += (predicted == labels).sum().item()
+            running_loss += loss.item()                    # adds error loss weight and sees total
+            _, predicted = torch.max(outputs.data, 1)     # figures out what the model thinks it is
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
 
-    epoch_accuracy = 100 * correct / total         # figures out accuracy of model
-    epoch_loss = running_loss / len(train_loader)  # figures out loss of model
+        epoch_accuracy = 100 * correct / total         # figures out accuracy of model
+        epoch_loss = running_loss / len(train_loader)  # figures out loss of model
 
-    model.eval() # switches off dropout so we get a clean honest measurement
-    val_loss = 0.0
-    val_correct = 0
-    val_total = 0
+        model.eval() # switches off dropout so we get a clean honest measurement
+        val_loss = 0.0
+        val_correct = 0
+        val_total = 0
 
-    with torch.no_grad(): # no need to calculate gradients during validation, saves memory
-        for images, labels in val_loader:
-            images, labels = images.to(device), labels.to(device)
-            outputs = model(images)
-            loss = criterion(outputs, labels)
-            val_loss += loss.item()
-            _, predicted = torch.max(outputs.data, 1) # figures out what the model thinks it is
-            val_total += labels.size(0)
-            val_correct += (predicted == labels).sum().item()
+        with torch.no_grad(): # no need to calculate gradients during validation, saves memory
+            for images, labels in val_loader:
+                images, labels = images.to(device), labels.to(device)
+                outputs = model(images)
+                loss = criterion(outputs, labels)
+                val_loss += loss.item()
+                _, predicted = torch.max(outputs.data, 1) # figures out what the model thinks it is
+                val_total += labels.size(0)
+                val_correct += (predicted == labels).sum().item()
 
-    val_accuracy = 100 * val_correct / val_total   # figures out validation accuracy
-    val_loss = val_loss / len(val_loader)           # figures out validation loss
+        val_accuracy = 100 * val_correct / val_total   # figures out validation accuracy
+        val_loss = val_loss / len(val_loader)           # figures out validation loss
 
-    scheduler.step(val_loss)               # modulates learning rate based on validation loss
-    current_lr = scheduler.get_last_lr()[0] # gets current learning rate
+        scheduler.step(val_loss)               # modulates learning rate based on validation loss
+        current_lr = scheduler.get_last_lr()[0] # gets current learning rate
 
-    print(f"\nDevice: {device}")
-    print(f"Learning Rate: {current_lr}")
-    print(f"Epoch {epoch+1}/{epochs} \n | Train Loss: {epoch_loss:.4f}, Train Acc: {epoch_accuracy:.2f}%\n | Val Loss: {val_loss:.4f},                   Val Acc: {val_accuracy:.2f}%")
+        print(f"\nDevice: {device}")
+        print(f"Learning Rate: {current_lr}")
+        print(f"Epoch {epoch+1}/{epochs} \n | Train Loss: {epoch_loss:.4f}, Train Acc: {epoch_accuracy:.2f}%\n | Val Loss: {val_loss:.4f},                   Val Acc: {val_accuracy:.2f}%")
 
-    if val_accuracy > best_accuracy: # if the model is better than the last one it saves it
-        best_accuracy = val_accuracy
-        torch.save({
-            "model_state": model.state_dict(), # save the weights
-            "accuracy": best_accuracy          # save the accuracy for next run
-        }, "best_character_model.pth")
-        print("------------------------ Breakthrough! Saved improved model! ------------------------")
+        if val_accuracy > best_accuracy: # if the model is better than the last one it saves it
+            best_accuracy = val_accuracy
+            torch.save({
+                "model_state": model.state_dict(), # save the weights
+                "accuracy": best_accuracy          # save the accuracy for next run
+            }, "best_character_model.pth")
+            print("------------------------ Breakthrough! Saved improved model! ------------------------")
 
-print("\nTraining complete!")
+    print("\nTraining complete!")
